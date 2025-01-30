af1a7f6d5d48ed68f53febd7165b9e6f8b53c226
koskedk <koskedk@live.com>
2025-01-30 21:01:33 +0300

    model testing

--- aide/jab.py
+++ aide/jab.py
@@ -2,10 +2,14 @@ import os
 from abc import ABC, abstractmethod
 
 from loguru import logger
-from ollama import ChatResponse, Client
+from ollama import ChatResponse, Client, ListResponse
 
 
 class Jab(ABC):
+    @abstractmethod
+    def get_models(self):
+        pass
+
     @abstractmethod
     def get_response(self):
         pass
@@ -23,9 +27,18 @@ class OllamaJab(Jab):
             logger.info(f"api :{self.api}")
             logger.info(f"model :{self.model}")
             logger.info("********************************")
+            self.client = Client(host=self.api)
         except Exception as e:
             logger.error(e)
             raise
+
+    def get_models(self):
+        self.client = Client(host=self.api)
+        list_response = self.client.list()
+        model_names = [model for model in list_response]
+        for m in model_names:
+            logger.info(f"{m}")
+        return model_names
 
     def ask(self, question: str, model: str = None):
         self.question = question
@@ -37,7 +50,7 @@ class OllamaJab(Jab):
         logger.info("********************************")
         logger.info("jab > {self.question}")
         logger.info("********************************")
-        self.client = Client(host=self.api)
+
         res: ChatResponse = self.client.chat(
             model=self.model,
             messages=[
--- tests/test_jab.py
+++ tests/test_jab.py
@@ -1,16 +1,26 @@
 import logging
-
+import pytest
 from loguru import logger
 
 from aide.jab import OllamaJab
 from core.config import initialize_app_testing
 
-
 ol = OllamaJab()
 
 
-def test_get_response():
-    ol.ask("does it snow in Nairobi?")
+def test_get_models():
+    mods = ol.get_models()
+    assert len(mods) > 0
+
+
+@pytest.mark.parametrize("model", [
+    "llama3.2:latest",
+    "deepseek-r1:7b",
+    "phi4:latest"
+])
+def test_get_response(model):
+    ol.ask("does it snow in Nairobi?", model)
     res = ol.get_response()
     assert res is not None
+    logger.info(f"||| {model} |||")
     logger.info(res)
